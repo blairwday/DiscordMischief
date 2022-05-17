ca1cdb9c19eec55067670aef36b03e4aebe4e280
blairwday <blair@blairday.com>
2022-05-17 08:18:54 -0700

    All NPM packages

--- packages.sh
+++ packages.sh
@@ -1,2 +1,12 @@
 wget -qO- https://deb.nodesource.com/setup_18.x | sudo -E bash
 sudo apt-get install -y nodejs
+npm install steam-user
+npm install steamcommunity
+npm install steam-totp
+npm install steam-tradeoffer-manager
+npm install globaloffensive
+npm install node-schedule
+npm install fs
+npm install sync-fetch
+npm install request
+npm install crypto
