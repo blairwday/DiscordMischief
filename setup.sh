de953e71b3f95eeb7bb527b638ff8fe279055de7
blairwday <blair@blairday.com>
2022-05-17 08:06:37 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ sudo apt-get update
 sudo apt-get -y dist-upgrade
 sudo apt-get -y upgrade
 sudo apt-get -y autoremove
+wget -qO- https://deb.nodesource.com/setup_18.x | sudo -E bash
+sudo apt-get install -y nodejs
